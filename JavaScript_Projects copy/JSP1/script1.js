f96363da6e5260b9a5a13e4d619afa953b48a0cb
Austin Torrey <artorrey22@gmail.com>
2016-09-11 19:26:48 -0500

    Initial Commit

---
+++ JavaScript_Projects copy/JSP1/script1.js
@@ -0,0 +1,20 @@
+
+
+
+function greetings() {
+    var Name = prompt ("Please enter your name");
+    var Snack = prompt ("What is your favorite snack?");
+    var Drink = prompt ("What is your favorite drink?");
+    if (Name != "") {
+        if (Snack != "") {
+            if (Drink != "") {
+                document.getElementById("first").innerHTML ="Hi there " + Name + ", it seems that you enjoy eating " + Snack + " and drinking " + Drink + "!";
+            }
+        }
+        
+    }
+}
+
+window.onload = greetings;
+
+
